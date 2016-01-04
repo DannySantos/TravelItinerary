c0aa6e4b953ef0bcb64740e0291345efcfb60abd
Simon Knight <simondknight1986@gmail.com>
2016-01-04 08:14:10 +0000

    Added test for creating a journey

--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Journey, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creating a journey" do
+    before do
+      Journey.create!(title: "Australia Holiday", description: "Going on holiday to Australia")
+    end
+
+    it "should create a journey" do
+      expect(Journey.last.title).to eq("Australia Holiday")
+      expect(Journey.last.description).to eq("Going on holiday to Australia")
+      expect(Journey.count).to eq(1)
+    end
+  end
 end
