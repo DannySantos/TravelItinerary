edce2c75f68269de8a19d9f96a9c5c0784bcb0ff
Simon Knight <simondknight1986@gmail.com>
2016-01-04 08:29:02 +0000

    Added test to test for title presence
    Added this to make sure the user has to enter a title when creating a new journey

--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe Journey, type: :model do
       expect(Journey.last.description).to eq("Going on holiday to Australia")
       expect(Journey.count).to eq(1)
     end
+
+    context "Missing title" do
+      before do
+      journey = Journey.create!(description: "Going on holiday to Australia")
+      end
+
+      it "should verify a title has been entered" do
+        expect(Journey.count).to eq(0)
+        expect {journey.save!}.to  raise_error(ActiveRecord::RecordInvalid,'Validation failed: Title can\'t be blank')
+      end
+    end
   end
 end
