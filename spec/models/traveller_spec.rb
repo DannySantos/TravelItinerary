a64da116e7491458f43c1fc68cb50095e65dfaba
Simon Knight <simondknight1986@gmail.com>
2016-01-05 09:16:17 +0000

    Added rspec tests for traveller registration

--- spec/models/traveller_spec.rb
+++ spec/models/traveller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Traveller, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creating a traveller" do 
+      before do
+        Traveller.create!(username: "vvhiteknight", email: "test@test.com", password: "password", password_confirmation: "password")
+      end
+
+      it "should create a traveller" do
+        expect(Traveller.last.username).to eq("vvhiteknight")
+        expect(Traveller.last.email).to eq("test@test.com")
+        expect(Traveller.all.length).to eq(1)
+      end
+
+      it "should verify that their username is unique" do
+        traveller = Traveller.new(username: "vvhiteknight", email: "test1@test.com", password: "password", password_confirmation: "password")
+        
+        expect {traveller.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Username has already been taken')
+        expect(Traveller.last.email).to eq("test@test.com")
+        expect(Traveller.all.length).to eq(1)
+      end
+
+      it "should verify the presense of a username" do
+        user = Traveller.new(email: "test1@test.com", password: "password", password_confirmation: "password")
+
+        expect {user.save!}.to  raise_error(ActiveRecord::RecordInvalid,'Validation failed: Username can\'t be blank')
+        expect(Traveller.last.email).to eq("test@test.com")
+        expect(Traveller.all.length).to eq(1)
+      end
+  end
 end
