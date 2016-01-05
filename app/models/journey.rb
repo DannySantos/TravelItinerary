33669e2e42314d8ffd6efc75df5b8bbbd91a087c
Simon Knight <simondknight1986@gmail.com>
2016-01-05 10:17:44 +0000

    Added relationships for traveller and journeys
    This will allow travellers to add journeys to their own ID

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,5 +1,8 @@
 class Journey < ActiveRecord::Base
 
+  has_one :traveller
+  belongs_to :traveller
+
   validates :title, presence: true
   validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105090739) do
+ActiveRecord::Schema.define(version: 20160105100640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "journeys", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "title"
     t.text     "description"
+    t.integer  "traveller_id"
   end
 
   create_table "travellers", force: :cascade do |t|
