97c2280f7ec995336f6433bd64da935af055beed
Simon Knight <simondknight1986@gmail.com>
2016-01-05 10:18:04 +0000

    Updated tests to reflect traveller / journey relationship

--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe Journey, type: :model do
   describe "Creating a journey" do
     before do
-      Journey.create!(title: "Australia Holiday", description: "Going on holiday to Australia")
+      @traveller = Traveller.create!(username: "vvhiteknight", email: "test@test.com", password: "password", password_confirmation: "password")
+      journey = Journey.create!(title: "Australia Holiday", description: "Going on holiday to Australia")
+      @traveller.journeys << journey
+
+      @traveller1 = Traveller.create!(username: "SimonKnight", email: "test1@test.com", password: "password", password_confirmation: "password")
+      journey1 = Journey.create!(title: "Maldives Holiday", description: "Going on holiday to Maldives")
+      @traveller1.journeys << journey1
     end
 
     it "should create a journey" do
-      expect(Journey.last.title).to eq("Australia Holiday")
-      expect(Journey.last.description).to eq("Going on holiday to Australia")
-      expect(Journey.count).to eq(1)
+      expect(@traveller.journeys.last.title).to eq("Australia Holiday")
+      expect(@traveller.journeys.last.description).to eq("Going on holiday to Australia")
+      expect(@traveller.journeys.count).to eq(1)
+
+      expect(@traveller1.journeys.last.title).to eq("Maldives Holiday")
+      expect(@traveller1.journeys.last.description).to eq("Going on holiday to Maldives")
+      expect(@traveller1.journeys.count).to eq(1)
     end
 
     context "Missing title" do
@@ -19,7 +29,7 @@ RSpec.describe Journey, type: :model do
 
       it "should verify a title has been entered" do
         expect {@journey.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Title can\'t be blank')
-        expect(Journey.count).to eq(1)
+        expect(Journey.count).to eq(2)
       end
     end
 
@@ -30,7 +40,7 @@ RSpec.describe Journey, type: :model do
 
       it "should verify a description has been entered" do
         expect {@journey.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Description can\'t be blank')
-        expect(Journey.count).to eq(1)
+        expect(Journey.count).to eq(2)
       end
     end
   end
