3cebf77992b9b7fcc55c7116153d4bb3cddb75ef
Simon Knight <simondknight1986@gmail.com>
2016-01-07 08:19:26 +0000

    Added testing for adding item

--- features/journey.feature
+++ features/journey.feature
@@ -2,6 +2,7 @@ Feature: Creating a journey
 
 @javascript
 Scenario: A customer creates a journey
+  Given a customer is signed in
   Given a customer is on the homepage
     And they select create a new journey
   Then they should be on the create new journey page
@@ -11,6 +12,7 @@ Scenario: A customer creates a journey
     And they are redirected to the journeys page
 
 Scenario: A customer creates a journey without a title
+  Given a customer is signed in 
   Given a customer is on the homepage
     And they select create a new journey
     And they enter a description
@@ -19,6 +21,7 @@ Scenario: A customer creates a journey without a title
     And they are shown a message saying "Please fill out all the fields"
 
 Scenario: A customer creates a journey without a description
+  Given a customer is signed in
   Given a customer is on the homepage
     And they select create a new journey
     And they enter a title
---
+++ features/step_definitions/add_todo_item_steps.rb
@@ -0,0 +1,39 @@
+
+Given(/^a traveller is signed in$/) do
+  @traveller = Traveller.create!(username: "vvhiteknight", email: "test@test.com", password: "password", password_confirmation: "password")
+  visit new_traveller_session_path
+  
+  fill_in "Email", with: "test@test.com"
+  fill_in "Password", with: "password"
+  click_button "Log in"
+end
+
+Given(/^a traveller has a journey$/) do
+  journey = Journey.create!(title: "Australia Holiday", description: "Going on holiday to Australia")
+  @traveller.journeys << journey
+  visit journeys_path
+end
+
+Given(/^they select to add a new todo item$/) do
+  click_link("Add New Todo Item")
+  sleep 1
+end
+
+Given(/^they enter valid todo item details$/) do
+  fill_in "Description", with: "Visit the crown casino"
+  sleep 1
+end
+
+Given(/^they they select the create item button$/) do
+  click_button("Create Item")
+  sleep 1
+end
+
+Then(/^a todo item should be created$/) do
+  expect(@traveller.journeys[0].items.first.description).to eq("Visit the crown casino")
+  expect(@traveller.journeys[0].items.count).to eq(1)
+end
+
+Then(/^they should see the todo item$/) do
+  expect(page).to have_content("Visit the crown casino")
+end
