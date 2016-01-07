a2f989fb96bba75fd9d5f3c2d8ac6e906f8a936f
Simon Knight <simondknight1986@gmail.com>
2016-01-07 10:55:58 +0000

    Add validations for dates
    Validates that an end date is not set before the start date

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,6 +3,14 @@ class Journey < ActiveRecord::Base
   belongs_to :traveller
   has_many :items
 
-  validates :title, presence: true
-  validates :description, presence: true
+  validates  :start_date, :end_date, :description, :title, presence: true
+  validate :end_date_cannot_be_before_start_date
+
+  def end_date_cannot_be_before_start_date
+    if start_date != nil && end_date != nil
+      if end_date < start_date
+      errors.add(:end_date, "can't be set before the start date")
+      end
+    end
+  end
 end
