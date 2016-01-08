6346af38a7a30217146419f8a3a9d730dbf9ccd9
Simon Knight <simondknight1986@gmail.com>
2016-01-08 10:58:43 +0000

    Added lat and lng to items database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107185030) do
+ActiveRecord::Schema.define(version: 20160108105645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20160107185030) do
     t.integer  "journey_id"
     t.string   "destination"
     t.text     "notes"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "journeys", force: :cascade do |t|
