de8e1fb47e3e49a3a847490d8aba2269216d7364
Simon Knight <simondknight1986@gmail.com>
2016-01-08 10:51:37 +0000

    Small addition to validation

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -2,10 +2,10 @@ class Journey < ActiveRecord::Base
 
   belongs_to :traveller
   has_many :items
-
   validates  :start_date, :end_date, :description, :title, presence: true
   validate :end_date_cannot_be_before_start_date
 
+  private
   def end_date_cannot_be_before_start_date
     if start_date != nil && end_date != nil
       if end_date < start_date
