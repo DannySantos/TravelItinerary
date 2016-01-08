fcd83996831957a219c2deed30af4c08abdeccbc
Simon Knight <simondknight1986@gmail.com>
2016-01-08 11:06:34 +0000

    Added address to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108105645) do
+ActiveRecord::Schema.define(version: 20160108110600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160108105645) do
     t.text     "notes"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "address"
   end
 
   create_table "journeys", force: :cascade do |t|
