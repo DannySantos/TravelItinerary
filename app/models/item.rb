76e5338ec179ac786c4c9399f8a14a71c2ffcd9c
Simon Knight <simondknight1986@gmail.com>
2016-01-08 11:27:53 +0000

    Added geocode to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :journey
-
   validates :description, :destination, presence: true
+
+  geocoded_by :geocode_string
+  after_validation :geocode 
+
+  def geocode_string
+    "#{destination}, #{address}"
+  end
 end
