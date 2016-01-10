91c24b9503cb2f59b3782783c5ac2be355cbe175
Simon Knight <simondknight1986@gmail.com>
2016-01-10 08:03:50 +0000

    Made changes to map implementation
    Moving away from map modal to full screen map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
 var map;
+var myLatLng = {lat: -25.363, lng: 131.044};
+
 function initMap() {
-  console.log("Hello from   MAPPY")
-  google.maps.event.trigger(map, 'resize');
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
