48c48879d2ab3129bd6675ccd6ef86d03183044e
Simon Knight <simondknight1986@gmail.com>
2016-01-12 18:33:43 +0000

    Stubbed out rspec test for facebook signup

--- spec/models/traveller_spec.rb
+++ spec/models/traveller_spec.rb
@@ -41,4 +41,31 @@ RSpec.describe Traveller, type: :model do
         expect(Traveller.all.length).to eq(1)
       end
   end
+
+  describe "facebook signup" do
+    before(:each) do
+      OmniAuth.config.mock_auth[:facebook] = nil
+    end
+
+    before do
+      OmniAuth.config.test_mode = true
+
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({"provider"=>"facebook",
+                       "uid"=>"1699874960291231",
+                       "info"=>{"email"=>"test5@test.com", "name"=>"Simon Knight", "image"=>"http://graph.facebook.com/169987434491231/picture"},
+                       "credentials"=>
+                        {"token"=>
+                          "12345",
+                         "expires_at"=>1457803746,
+                         "expires"=>true},
+                       "extra"=>{"raw_info"=>{"name"=>"Simon Knight", "email"=>"simon.mobilep@gmail.com", "id"=>"12345"}}})
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+    end
+
+    it "should successfully create a user" do
+      @traveller = Traveller.from_omniauth(Rails.application.env_config["omniauth.auth"])
+      expect(Traveller.count).to eq(1)
+      expect(Traveller.last.email).to eq("test5@test.com")
+    end
+  end
 end
